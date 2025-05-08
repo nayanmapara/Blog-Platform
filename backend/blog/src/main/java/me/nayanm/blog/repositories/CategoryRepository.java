b05dc4131d4a68d091a7ff9c2db31f034fbfdf41
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-08 19:11:16 -0400

    feat: implement CategoryRepository interface extending JpaRepository for category entity management

--- backend/blog/src/main/java/me/nayanm/blog/repositories/CategoryRepository.java
+++ backend/blog/src/main/java/me/nayanm/blog/repositories/CategoryRepository.java
@@ -1,4 +1,12 @@
 package me.nayanm.blog.repositories;
 
-public interface CategoryRepository {
+import me.nayanm.blog.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    
 }
