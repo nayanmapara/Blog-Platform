1d2b3b5516fdb3e220d6f1a3a9e6cef462eb3b6e
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-08 19:08:35 -0400

    feat: implement UserRepository interface extending JpaRepository for user entity management

--- backend/blog/src/main/java/me/nayanm/blog/repositories/UserRepository.java
+++ backend/blog/src/main/java/me/nayanm/blog/repositories/UserRepository.java
@@ -1,4 +1,12 @@
 package me.nayanm.blog.repositories;
 
-public interface UserRepository {
+import me.nayanm.blog.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    
 }
