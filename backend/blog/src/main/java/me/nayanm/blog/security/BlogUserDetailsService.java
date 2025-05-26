a15058e5d00fed9ead1f278da311ee054fa505d1
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-26 11:27:04 -0400

    feat: implement BlogUserDetailsService to load user details by email

--- backend/blog/src/main/java/me/nayanm/blog/security/BlogUserDetailsService.java
+++ backend/blog/src/main/java/me/nayanm/blog/security/BlogUserDetailsService.java
@@ -1,4 +1,22 @@
 package me.nayanm.blog.security;
 
-public class BlogUserDetailsService {
+import lombok.RequiredArgsConstructor;
+
+import me.nayanm.blog.domain.entities.User;
+import me.nayanm.blog.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new BlogUserDetails(user);
+    }
 }
