7808e08744fd9165c3b9d76c969f87619b441c51
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-27 04:51:54 -0400

    feat: implement AuthenticationServiceImpl class with authentication methods

--- backend/blog/src/main/java/me/nayanm/blog/services/impl/AuthenticationServiceImpl.java
+++ backend/blog/src/main/java/me/nayanm/blog/services/impl/AuthenticationServiceImpl.java
@@ -1,4 +1,20 @@
 package me.nayanm.blog.services.impl;
 
-public class AuthenticationServiceImpl {
+import lombok.RequiredArgsConstructor;
+import me.nayanm.blog.services.AuthenticationService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Override
+    public UserDetails authenticate(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public String generateToken(UserDetails userDetails) {
+        return "";
+    }
 }
