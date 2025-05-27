e5a78ed690a57da572bc3da293d0638694912998
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-27 05:03:32 -0400

    feat: implement authentication methods in AuthenticationServiceImpl and add JWT secret to application properties

--- backend/blog/src/main/java/me/nayanm/blog/services/impl/AuthenticationServiceImpl.java
+++ backend/blog/src/main/java/me/nayanm/blog/services/impl/AuthenticationServiceImpl.java
@@ -1,20 +1,54 @@
 package me.nayanm.blog.services.impl;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import me.nayanm.blog.services.AuthenticationService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private final Long jwtExpiryMs = 86400000L;
+
     @Override
     public UserDetails authenticate(String email, String password) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        return userDetailsService.loadUserByUsername(email);
     }
 
     @Override
     public String generateToken(UserDetails userDetails) {
-        return "";
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiryMs))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = secretKey.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
