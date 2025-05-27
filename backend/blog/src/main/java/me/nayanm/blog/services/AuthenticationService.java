e8753efdda0a905c87013ab45bc86a316cc51c8f
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-27 04:45:21 -0400

    feat: add authentication methods to AuthenticationService interface

--- backend/blog/src/main/java/me/nayanm/blog/services/AuthenticationService.java
+++ backend/blog/src/main/java/me/nayanm/blog/services/AuthenticationService.java
@@ -1,4 +1,8 @@
 package me.nayanm.blog.services;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface AuthenticationService {
+    UserDetails authenticate(String email, String password);
+    String generateToken(UserDetails userDetails);
 }
