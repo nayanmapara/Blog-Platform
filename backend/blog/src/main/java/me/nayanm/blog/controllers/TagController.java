3363ef026d7962d7bc5f55e331ef13b2731a4dc9
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-28 09:50:24 -0400

    feat: implement getAllTags endpoint in TagController for fetching all tags

--- backend/blog/src/main/java/me/nayanm/blog/controllers/TagController.java
+++ backend/blog/src/main/java/me/nayanm/blog/controllers/TagController.java
@@ -1,4 +1,29 @@
 package me.nayanm.blog.controllers;
 
+import lombok.RequiredArgsConstructor;
+import me.nayanm.blog.domain.dtos.TagResponse;
+import me.nayanm.blog.domain.entities.Tag;
+import me.nayanm.blog.mappers.TagMapper;
+import me.nayanm.blog.services.TagService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/tags")
+@RequiredArgsConstructor
 public class TagController {
+
+    private final TagService tagService;
+    private final TagMapper tagMapper;
+
+    @GetMapping
+    public ResponseEntity<List<TagResponse>> getAllTags(){
+        List<Tag> tags = tagService.getTags();
+        List<TagResponse> tagResponses = tags.stream().map(tagMapper::toTagResponse).toList();
+        return ResponseEntity.ok(tagResponses);
+    }
 }
