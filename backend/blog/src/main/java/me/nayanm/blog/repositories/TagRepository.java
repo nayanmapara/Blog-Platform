f4e79bfd41d26a09a60a164f6fbd53de04840b05
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-28 09:50:17 -0400

    feat: add findAllWithPostCount method to TagRepository for fetching tags with post count

--- backend/blog/src/main/java/me/nayanm/blog/repositories/TagRepository.java
+++ backend/blog/src/main/java/me/nayanm/blog/repositories/TagRepository.java
@@ -2,11 +2,16 @@ package me.nayanm.blog.repositories;
 
 import me.nayanm.blog.domain.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, UUID> {
+
+    @Query("SELECT t FROM Tag t LEFT JOIN FETCH t.posts")
+    List<Tag> findAllWithPostCount();
 
 }
