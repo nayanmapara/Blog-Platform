7885be557b34fe87edd18c64051e08f817e968fd
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-28 10:16:17 -0400

    feat: refactor TagController to improve import organization and formatting

--- backend/blog/src/main/java/me/nayanm/blog/controllers/TagController.java
+++ backend/blog/src/main/java/me/nayanm/blog/controllers/TagController.java
@@ -1,14 +1,14 @@
 package me.nayanm.blog.controllers;
 
 import lombok.RequiredArgsConstructor;
+import me.nayanm.blog.domain.dtos.CreateTagsRequest;
 import me.nayanm.blog.domain.dtos.TagResponse;
 import me.nayanm.blog.domain.entities.Tag;
 import me.nayanm.blog.mappers.TagMapper;
 import me.nayanm.blog.services.TagService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +25,15 @@ public class TagController {
         List<Tag> tags = tagService.getTags();
         List<TagResponse> tagResponses = tags.stream().map(tagMapper::toTagResponse).toList();
         return ResponseEntity.ok(tagResponses);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TagResponse>> createTags(@RequestBody CreateTagsRequest createTagsRequest){
+        List<Tag> savedTags = tagService.createTags(createTagsRequest.getNames());
+        List<TagResponse> createdTagResponses = savedTags.stream().map(tagMapper::toTagResponse).toList();
+        return new ResponseEntity<>(
+                createdTagResponses,
+                HttpStatus.CREATED
+        );
     }
 }
