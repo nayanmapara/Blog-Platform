f197d52fea1de3b69f76a9a10a5c25c50e3d005c
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-28 09:50:12 -0400

    feat: implement TagServiceImpl with getTags method and repository integration

--- backend/blog/src/main/java/me/nayanm/blog/services/impl/TagServiceImpl.java
+++ backend/blog/src/main/java/me/nayanm/blog/services/impl/TagServiceImpl.java
@@ -1,4 +1,21 @@
 package me.nayanm.blog.services.impl;
 
-public class TagServiceImpl {
+import lombok.RequiredArgsConstructor;
+import me.nayanm.blog.domain.entities.Tag;
+import me.nayanm.blog.repositories.TagRepository;
+import me.nayanm.blog.services.TagService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TagServiceImpl implements TagService {
+
+    private final TagRepository tagRepository;
+
+    @Override
+    public List<Tag> getTags() {
+        return tagRepository.findAllWithPostCount();
+    }
 }
