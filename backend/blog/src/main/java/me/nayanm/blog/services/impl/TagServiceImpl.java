7e0b8b7a685338ebeaaf912e4c24862216210432
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-28 10:16:13 -0400

    feat: implement createTags method in TagService for batch tag creation

--- backend/blog/src/main/java/me/nayanm/blog/services/impl/TagServiceImpl.java
+++ backend/blog/src/main/java/me/nayanm/blog/services/impl/TagServiceImpl.java
@@ -1,12 +1,17 @@
 package me.nayanm.blog.services.impl;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import me.nayanm.blog.domain.entities.Tag;
 import me.nayanm.blog.repositories.TagRepository;
 import me.nayanm.blog.services.TagService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +22,32 @@ public class TagServiceImpl implements TagService {
     @Override
     public List<Tag> getTags() {
         return tagRepository.findAllWithPostCount();
+    }
+
+    @Transactional
+    @Override
+    public List<Tag> createTags(Set<String> tagNames) {
+        List<Tag> existingTags = tagRepository.findByNameIn(tagNames);
+
+        Set<String> existingTagNames = existingTags.stream()
+                .map(Tag::getName)
+                .collect(Collectors.toSet());
+
+        List<Tag> newTags = tagNames.stream()
+                .filter(name -> !existingTagNames.contains(name))
+                .map(name -> Tag.builder()
+                        .name(name)
+                        .posts(new HashSet<>())
+                        .build())
+                .toList();
+
+        List<Tag> savedTags = new ArrayList<>();
+        if (!newTags.isEmpty()){
+            tagRepository.saveAll(newTags);
+        }
+
+        savedTags.addAll(existingTags);
+
+        return savedTags;
     }
 }
