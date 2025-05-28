648529771a2401e91435fd6e3ad3898f83d9afa7
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-28 10:29:38 -0400

    feat: add deleteTag endpoint to TagController for tag removal by ID

--- backend/blog/src/main/java/me/nayanm/blog/controllers/TagController.java
+++ backend/blog/src/main/java/me/nayanm/blog/controllers/TagController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/api/v1/tags")
@@ -35,5 +36,11 @@ public class TagController {
                 createdTagResponses,
                 HttpStatus.CREATED
         );
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteTag(@PathVariable UUID id) {
+        tagService.deleteTag(id);
+        return ResponseEntity.noContent().build();
     }
 }
