1c70c0f682025e61f8e7a441efca59780fa91dfa
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-28 10:16:08 -0400

    feat: add createTags method to TagService for creating tags from a set of names

--- backend/blog/src/main/java/me/nayanm/blog/services/TagService.java
+++ backend/blog/src/main/java/me/nayanm/blog/services/TagService.java
@@ -3,7 +3,9 @@ package me.nayanm.blog.services;
 import me.nayanm.blog.domain.entities.Tag;
 
 import java.util.List;
+import java.util.Set;
 
 public interface TagService {
     List<Tag> getTags();
+    List<Tag> createTags(Set<String> tagNames);
 }
