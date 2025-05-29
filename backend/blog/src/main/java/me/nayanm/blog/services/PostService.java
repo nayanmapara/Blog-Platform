3b9a76250e993030bc3fcc54436ea89ac5101605
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-29 00:35:20 -0400

    feat: add getAllPosts method to PostService interface

--- backend/blog/src/main/java/me/nayanm/blog/services/PostService.java
+++ backend/blog/src/main/java/me/nayanm/blog/services/PostService.java
@@ -1,4 +1,10 @@
 package me.nayanm.blog.services;
 
+import me.nayanm.blog.domain.entities.Post;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface PostService {
+    List<Post> getAllPosts(UUID categoryId, UUID tagId);
 }
