5cd0f186218a907bfb3e10fd847534558bd07d9b
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-29 00:34:52 -0400

    feat: add query methods to PostRepository for filtering posts by status, category, and tags

--- backend/blog/src/main/java/me/nayanm/blog/repositories/PostRepository.java
+++ backend/blog/src/main/java/me/nayanm/blog/repositories/PostRepository.java
@@ -1,12 +1,20 @@
 package me.nayanm.blog.repositories;
 
+import me.nayanm.blog.domain.PostStatus;
+import me.nayanm.blog.domain.entities.Category;
 import me.nayanm.blog.domain.entities.Post;
+import me.nayanm.blog.domain.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
-
+    List<Post> findAllByStatusAndCategoryAndTagsContaining(PostStatus status, Category category, Tag tags);
+    List<Post> findAllByStatusAndCategory(PostStatus status, Category category);
+    List<Post> findAllByStatusAndTagsContaining(PostStatus status, Tag tags);
+    List<Post> findAllByStatus(PostStatus status);
 }
