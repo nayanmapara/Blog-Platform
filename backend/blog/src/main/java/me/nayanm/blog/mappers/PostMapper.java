37f7bb369e26eb87bd9071f03b17538e45885b85
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-29 00:34:59 -0400

    feat: refactor PostMapper to improve readability and maintainability

--- backend/blog/src/main/java/me/nayanm/blog/mappers/PostMapper.java
+++ backend/blog/src/main/java/me/nayanm/blog/mappers/PostMapper.java
@@ -1,4 +1,17 @@
 package me.nayanm.blog.mappers;
 
+import me.nayanm.blog.domain.dtos.PostDto;
+import me.nayanm.blog.domain.entities.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PostMapper {
+
+    @Mapping(target = "category", source = "category")
+    @Mapping(target = "tags", source = "tags")
+    @Mapping(target = "author", source = "author")
+    PostDto toDto(Post post);
+
 }
