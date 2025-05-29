ccc06a4ccc5088d64d8b6a7ff08bfeacf878c846
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-29 00:35:26 -0400

    feat: implement getAllPosts method in PostServiceImpl for filtering posts by category and tag

--- backend/blog/src/main/java/me/nayanm/blog/services/impl/PostServiceImpl.java
+++ backend/blog/src/main/java/me/nayanm/blog/services/impl/PostServiceImpl.java
@@ -1,4 +1,49 @@
 package me.nayanm.blog.services.impl;
 
-public class PostServiceImpl {
+import lombok.RequiredArgsConstructor;
+import me.nayanm.blog.domain.PostStatus;
+import me.nayanm.blog.domain.entities.Category;
+import me.nayanm.blog.domain.entities.Post;
+import me.nayanm.blog.domain.entities.Tag;
+import me.nayanm.blog.repositories.PostRepository;
+import me.nayanm.blog.services.PostService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+    private final CategoryServiceImpl categoryService;
+    private final TagServiceImpl tagService;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Post> getAllPosts(UUID categoryId, UUID tagId) {
+        if(categoryId != null && tagId != null){
+            Category category = categoryService.getCategoryById(categoryId);
+            Tag tag = tagService.getTagById(tagId);
+            return postRepository.findAllByStatusAndCategoryAndTagsContaining(
+                    PostStatus.PUBLISHED,
+                    category,
+                    tag
+            );
+        }
+
+        if (categoryId != null) {
+            Category category = categoryService.getCategoryById(categoryId);
+            return postRepository.findAllByStatusAndCategory(PostStatus.PUBLISHED, category);
+        }
+
+        if (tagId != null) {
+            Tag tag = tagService.getTagById(tagId);
+            return postRepository.findAllByStatusAndTagsContaining(PostStatus.PUBLISHED, tag);
+        }
+
+        return postRepository.findAllByStatus(PostStatus.PUBLISHED);
+    }
 }
