425acf8ef4e3e9a6c55c8b7c7f5994f62b413e35
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-29 00:34:44 -0400

    feat: add PostDto class with fields for id, title, content, author, category, tags, readingTime, createdAt, updatedAt, and postStatus

--- backend/blog/src/main/java/me/nayanm/blog/domain/dtos/PostDto.java
+++ backend/blog/src/main/java/me/nayanm/blog/domain/dtos/PostDto.java
@@ -1,4 +1,29 @@
 package me.nayanm.blog.domain.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.nayanm.blog.domain.PostStatus;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PostDto {
+    private UUID id;
+    private String title;
+    private String content;
+    private AuthorDto author;
+    private CategoryDto category;
+    private Set<TagDto> tags;
+    private Integer readingTime;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private PostStatus postStatus;
+
 }
