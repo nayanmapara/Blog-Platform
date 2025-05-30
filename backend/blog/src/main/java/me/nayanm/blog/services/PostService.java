6f5eed08045b259b12d848914401cb7bd12d2b27
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-30 05:31:22 -0400

    feat: add methods to retrieve draft posts and create a post in PostService

--- backend/blog/src/main/java/me/nayanm/blog/services/PostService.java
+++ backend/blog/src/main/java/me/nayanm/blog/services/PostService.java
@@ -1,10 +1,14 @@
 package me.nayanm.blog.services;
 
+import me.nayanm.blog.domain.CreatePostRequest;
 import me.nayanm.blog.domain.entities.Post;
+import me.nayanm.blog.domain.entities.User;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface PostService {
     List<Post> getAllPosts(UUID categoryId, UUID tagId);
+    List<Post> getDraftPosts(User user);
+    Post createPost(User user, CreatePostRequest createPostRequest);
 }
