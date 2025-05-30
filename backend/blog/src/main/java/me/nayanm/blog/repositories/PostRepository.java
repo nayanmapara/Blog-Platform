fc18f81a1176b37a8e722afcf9ae64629199ce5b
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-30 05:31:16 -0400

    feat: add method to find posts by author and status in PostRepository

--- backend/blog/src/main/java/me/nayanm/blog/repositories/PostRepository.java
+++ backend/blog/src/main/java/me/nayanm/blog/repositories/PostRepository.java
@@ -4,6 +4,7 @@ import me.nayanm.blog.domain.PostStatus;
 import me.nayanm.blog.domain.entities.Category;
 import me.nayanm.blog.domain.entities.Post;
 import me.nayanm.blog.domain.entities.Tag;
+import me.nayanm.blog.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -17,4 +18,5 @@ public interface PostRepository extends JpaRepository<Post, UUID> {
     List<Post> findAllByStatusAndCategory(PostStatus status, Category category);
     List<Post> findAllByStatusAndTagsContaining(PostStatus status, Tag tags);
     List<Post> findAllByStatus(PostStatus status);
+    List<Post> findAllByAuthorAndStatus(User author, PostStatus status);
 }
