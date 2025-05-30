db9613314ee15293b8c1b2a473dd9db2868894c2
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-30 05:31:36 -0400

    feat: implement createPost method and calculateReadingTime utility in PostServiceImpl

--- backend/blog/src/main/java/me/nayanm/blog/services/impl/PostServiceImpl.java
+++ backend/blog/src/main/java/me/nayanm/blog/services/impl/PostServiceImpl.java
@@ -1,16 +1,20 @@
 package me.nayanm.blog.services.impl;
 
 import lombok.RequiredArgsConstructor;
+import me.nayanm.blog.domain.CreatePostRequest;
 import me.nayanm.blog.domain.PostStatus;
 import me.nayanm.blog.domain.entities.Category;
 import me.nayanm.blog.domain.entities.Post;
 import me.nayanm.blog.domain.entities.Tag;
+import me.nayanm.blog.domain.entities.User;
 import me.nayanm.blog.repositories.PostRepository;
 import me.nayanm.blog.services.PostService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -20,6 +24,8 @@ public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
     private final CategoryServiceImpl categoryService;
     private final TagServiceImpl tagService;
+
+    private static final int WORDS_PER_MINUTE = 200;
 
     @Override
     @Transactional(readOnly = true)
@@ -45,5 +51,39 @@ public class PostServiceImpl implements PostService {
         }
 
         return postRepository.findAllByStatus(PostStatus.PUBLISHED);
+    }
+
+    @Override
+    public List<Post> getDraftPosts(User user) {
+        return postRepository.findAllByAuthorAndStatus(user, PostStatus.DRAFT);
+    }
+
+    @Override
+    @Transactional
+    public Post createPost(User user, CreatePostRequest createPostRequest) {
+        Post newPost = new Post();
+        newPost.setTitle(createPostRequest.getTitle());
+        newPost.setContent(createPostRequest.getContent());
+        newPost.setStatus(createPostRequest.getStatus());
+        newPost.setAuthor(user);
+        newPost.setReadingTime(calculateReadingTime(createPostRequest.getContent()));
+
+        Category category = categoryService.getCategoryById(createPostRequest.getCategoryId());
+        newPost.setCategory(category);
+
+        Set<UUID> tagIds = createPostRequest.getTagIds();
+        List<Tag> tags = tagService.getTagsByIds(tagIds);
+        newPost.setTags(new HashSet<>(tags));
+
+        return postRepository.save(newPost);
+    }
+
+    private Integer calculateReadingTime(String content){
+        if (content == null || content.isEmpty()) {
+            return 0;
+        }
+
+        int wordCount = content.trim().split("\\s+").length;
+        return (int) Math.ceil((double) wordCount / WORDS_PER_MINUTE);
     }
 }
