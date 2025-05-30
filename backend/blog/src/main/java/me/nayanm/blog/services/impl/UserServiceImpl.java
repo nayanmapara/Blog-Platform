f32ce378f476c8015415e15a8deff21b56986cf1
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-30 05:31:46 -0400

    feat: implement UserServiceImpl with getUserById method

--- backend/blog/src/main/java/me/nayanm/blog/services/impl/UserServiceImpl.java
+++ backend/blog/src/main/java/me/nayanm/blog/services/impl/UserServiceImpl.java
@@ -1,4 +1,24 @@
 package me.nayanm.blog.services.impl;
 
-public class UserServiceImpl {
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import me.nayanm.blog.domain.entities.User;
+import me.nayanm.blog.repositories.UserRepository;
+import me.nayanm.blog.services.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User getUserById(UUID id) {
+        return userRepository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + id));
+    }
 }
