708ca05e06240cb411bf7acc0ae871b9e3e210ab
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-31 10:32:38 -0400

    feat: add updatePost method to PostService interface for post updates

--- backend/blog/src/main/java/me/nayanm/blog/services/PostService.java
+++ backend/blog/src/main/java/me/nayanm/blog/services/PostService.java
@@ -1,8 +1,10 @@
 package me.nayanm.blog.services;
 
 import me.nayanm.blog.domain.CreatePostRequest;
+import me.nayanm.blog.domain.UpdatePostRequest;
 import me.nayanm.blog.domain.entities.Post;
 import me.nayanm.blog.domain.entities.User;
+import org.springframework.security.core.parameters.P;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,4 +13,5 @@ public interface PostService {
     List<Post> getAllPosts(UUID categoryId, UUID tagId);
     List<Post> getDraftPosts(User user);
     Post createPost(User user, CreatePostRequest createPostRequest);
+    Post updatePost(UUID id, UpdatePostRequest updatePostRequest);
 }
