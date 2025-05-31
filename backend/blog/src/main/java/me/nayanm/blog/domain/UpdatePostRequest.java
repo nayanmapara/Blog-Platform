4d4b82742e8e61df51a5ef411bb6145dbd205525
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-05-31 09:57:39 -0400

    feat: refactor UpdatePostRequest class to include validation annotations and improve structure

--- backend/blog/src/main/java/me/nayanm/blog/domain/UpdatePostRequest.java
+++ backend/blog/src/main/java/me/nayanm/blog/domain/UpdatePostRequest.java
@@ -1,4 +1,33 @@
 package me.nayanm.blog.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UpdatePostRequest {
+
+    private UUID id;
+
+    private String title;
+
+    private String content;
+
+    private UUID categoryId;
+
+    @Builder.Default
+    private Set<UUID> tagIds = new HashSet<>();
+
+    private PostStatus postStatus;
 }
