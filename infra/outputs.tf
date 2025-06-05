c1c63a9e2729591f8a5313d77a1be7bac7d2d9a1
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-06-05 08:40:14 -0400

    refactor: add frontend storage account and update outputs, enhance variable definitions

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,13 @@
-output "app_url" {
+output "backend_url" {
   description = "The URL of the deployed Spring Boot API"
   value       = azurerm_linux_web_app.backend.default_hostname
 }
+
+output "frontend_url" {
+    description = "The URL of the deployed frontend static website"
+    value = azurerm_storage_account.frontend.primary_web_endpoint
+}
+
 
 output "postgres_fqdn" {
   description = "PostgreSQL database FQDN"
--- infra/variables.tf
+++ infra/variables.tf
@@ -19,3 +19,24 @@ variable "jwt_secret" {
   type        = string
   sensitive   = true
 }
+
+variable "vite_api_base_url" {
+  description = "The base URL of the backend API for the frontend to consume"
+  type        = string
+}
+
+variable "mime_types" {
+  description = "Map of file extensions to MIME types"
+  type = map(string)
+  default = {
+    html = "text/html"
+    css  = "text/css"
+    js   = "application/javascript"
+    json = "application/json"
+    png  = "image/png"
+    svg  = "image/svg+xml"
+    jpg  = "image/jpeg"
+    jpeg = "image/jpeg"
+    ico  = "image/x-icon"
+  }
+}
