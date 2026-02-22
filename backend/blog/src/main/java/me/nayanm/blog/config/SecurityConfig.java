1046eb0724f8751e70b9659549b17d88491e1d00
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2026-02-22 16:00:59 -0500

    fix: update CORS configuration to allow additional frontend URL

--- backend/blog/src/main/java/me/nayanm/blog/config/SecurityConfig.java
+++ backend/blog/src/main/java/me/nayanm/blog/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://blogfrontend.z9.web.core.windows.net"));
+        config.setAllowedOrigins(List.of("https://blogfrontend.z9.web.core.windows.net", "https://blog-platform-tpcg.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- backend/blog/src/main/java/me/nayanm/blog/config/WebConfig.java
+++ backend/blog/src/main/java/me/nayanm/blog/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply CORS settings to all endpoints
-                .allowedOrigins("https://blogfrontend.z9.web.core.windows.net") // Replace with your frontend's URL
+                .allowedOrigins("https://blogfrontend.z9.web.core.windows.net", "https://blog-platform-tpcg.onrender.com") // Replace with your frontend's URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials (e.g., cookies)
