8f0a59acd3d00e8914f349f09b4bcdd0b8b395c9
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2026-02-22 15:03:31 -0500

    feat: update CI/CD workflows for backend and frontend deployment to Azure

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(() => {
     server: {
       proxy: {
         '/api': {
-          target: "https://blog-platform-api.azurewebsites.net/",
+          target: "https://blog-app-latest-1.onrender.com/",
           changeOrigin: true,
           secure: false,
         },
